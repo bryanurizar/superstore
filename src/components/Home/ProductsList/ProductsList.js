abbdd50e494e7f86e7dfd726b5776068a0e6e008
Bryan Urizar <bryan.urizar@outlook.com>
2021-08-02 17:08:59 +0100

    changes?

--- src/components/Home/ProductsList/ProductsList.js
+++ src/components/Home/ProductsList/ProductsList.js
@@ -11,11 +11,10 @@ async function getProductData(url) {
 function ProductsList() {
   const [products, setProducts] = useState([]);
 
-  useEffect( () => {
-    getProductData('https://gp-super-store-api.herokuapp.com/item/list')
-      .then(productData => {
-        setProducts(productData.items);
-    })
+  useEffect(() => {
+    getProductData("https://gp-super-store-api.herokuapp.com/item/list")
+      .then((data) => setProducts(data.items))
+      .catch(console.err);
   }, []);
 
   console.log(products);
@@ -23,22 +22,21 @@ function ProductsList() {
   return (
     <section className="products-section">
       <div className="products-container">
-        {products.map(product => {
+        {products.map((product) => {
           return (
-          <ProductItem
-            avgRating={product.avgRating}
-            description={product.description}
-            imageUrl={product.imageUrl}
-            isOnSale={product.isOnSale}
-            name={product.name}
-            price={product.price}
-            stockCount={product.stockCount}
-            key={product._id}
-           />
-          )
-        })
-      }
-        </div>
+            <ProductItem
+              avgRating={product.avgRating}
+              description={product.description}
+              imageUrl={product.imageUrl}
+              isOnSale={product.isOnSale}
+              name={product.name}
+              price={product.price}
+              stockCount={product.stockCount}
+              key={product._id}
+            />
+          );
+        })}
+      </div>
     </section>
   );
 }
